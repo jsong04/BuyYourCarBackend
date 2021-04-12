8b0c04cde57e2f7b098ff75fc97dfad7829be067
JingZheng <song.jingz@husky.neu.edu>
2021-04-12 19:08:42 +0800

    backend project setup

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const categories = require("./routes/categories");
+const listings = require("./routes/listings");
+const listing = require("./routes/listing");
+const users = require("./routes/users");
+const user = require("./routes/user");
+const auth = require("./routes/auth");
+const my = require("./routes/my");
+const messages = require("./routes/messages");
+const search = require('./routes/search');
+const expoPushTokens = require("./routes/expoPushTokens");
+const helmet = require("helmet");
+const compression = require("compression");
+const config = require("config");
+const bodyParser = require('body-parser');
+const formidableMiddleware = require('express-formidable');
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(helmet());
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(formidableMiddleware());
+
+app.use("/api/categories", categories);
+app.use("/api/listing", listing);
+app.use("/api/listings", listings);
+app.use("/api/user", user);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/my", my);
+app.use("/api/expoPushTokens", expoPushTokens);
+app.use("/api/messages", messages);
+app.use("/api/search", search);
+
+const port = process.env.PORT || config.get("port");
+app.listen(port, function() {
+  console.log(`Server started on port ${port}...`);
+});
